6f8d4ba8db1874e1fa7317b4802957124a16b814
Deepali Sharma <deepalisharma@Deepalis-MacBook-Air-2.local>
2025-07-19 18:53:21 +0800

    fix: remove all references to old audioSlice and cleanup imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,131 +1,132 @@
-import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { 
-  Container, 
-  Box, 
-  Snackbar, 
-  Alert,
-  Fab,
-  useTheme,
-  useMediaQuery
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link as RouterLink,
+} from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
 } from '@mui/material';
-import { Mic, List, History } from '@mui/icons-material';
-
+import {
+  Home,
+  ListAlt,
+  Menu as MenuIcon,
+  Settings,
+  Info,
+} from '@mui/icons-material';
 import HomePage from './pages/HomePage';
 import ShoppingListPage from './pages/ShoppingListPage';
-import HistoryPage from './pages/HistoryPage';
-import BottomNavigation from './components/navigation/BottomNavigation';
-import AudioRecorder from './components/audio/AudioRecorder';
-import AddItemModal from './components/modals/AddItemModal';
-import EditItemModal from './components/modals/EditItemModal';
-
+import { useDispatch } from 'react-redux';
 import { fetchItems } from './store/slices/itemsSlice';
-import { hideNotification, setCurrentTab } from './store/slices/uiSlice';
-import { setIsSupported } from './store/slices/audioSlice';
+import ThemeProvider from './theme/ThemeProvider';
+import PWAInstaller from './components/PWAInstaller';
 
 function App() {
   const dispatch = useDispatch();
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-  
-  const { notification } = useSelector(state => state.ui);
-  const { isRecording } = useSelector(state => state.audio);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   useEffect(() => {
-    // Check for audio recording support
-    const checkAudioSupport = () => {
-      const hasGetUserMedia = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-      const hasMediaRecorder = !!window.MediaRecorder;
-      dispatch(setIsSupported(hasGetUserMedia && hasMediaRecorder));
-    };
-
-    checkAudioSupport();
-    
-    // Fetch initial data
     dispatch(fetchItems());
-
-    // Handle PWA installation
-    let deferredPrompt;
-    const handleBeforeInstallPrompt = (e) => {
-      e.preventDefault();
-      deferredPrompt = e;
-    };
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-
-    return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    };
   }, [dispatch]);
 
-  const handleCloseNotification = () => {
-    dispatch(hideNotification());
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
-    <Box sx={{ 
-      minHeight: '100vh', 
-      backgroundColor: 'background.default',
-      paddingBottom: isMobile ? '80px' : '20px',
-    }}>
-      <Container maxWidth="lg" sx={{ py: 2 }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shopping-list" element={<ShoppingListPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-        </Routes>
-      </Container>
-
-      {/* Bottom Navigation for Mobile */}
-      {isMobile && <BottomNavigation />}
-
-      {/* Floating Action Button for Quick Record (on mobile, when not on home page) */}
-      {isMobile && window.location.pathname !== '/' && (
-        <Fab
-          color="primary"
-          sx={{
-            position: 'fixed',
-            bottom: 90,
-            right: 16,
-            zIndex: 1000,
-            ...(isRecording && {
-              backgroundColor: 'error.main',
-              '&:hover': {
-                backgroundColor: 'error.dark',
-              },
-            }),
-          }}
-          onClick={() => dispatch(setCurrentTab('home'))}
-        >
-          <Mic />
-        </Fab>
-      )}
-
-      {/* Audio Recorder Component */}
-      <AudioRecorder />
-
-      {/* Modals */}
-      <AddItemModal />
-      <EditItemModal />
-
-      {/* Global Notification */}
-      <Snackbar
-        open={notification.open}
-        autoHideDuration={6000}
-        onClose={handleCloseNotification}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      >
-        <Alert
-          onClose={handleCloseNotification}
-          severity={notification.severity}
-          variant="filled"
-          sx={{ width: '100%' }}
-        >
-          {notification.message}
-        </Alert>
-      </Snackbar>
-    </Box>
+    <ThemeProvider>
+      <Router>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Button
+                color="inherit"
+                component={RouterLink}
+                to="/"
+                sx={{ textTransform: 'none', fontSize: '1.25rem' }}
+              >
+                PantryPilot
+              </Button>
+            </Typography>
+            <PWAInstaller />
+            <IconButton
+              color="inherit"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose} component={RouterLink} to="/">
+                <ListItemIcon>
+                  <Home fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Home</ListItemText>
+              </MenuItem>
+              <MenuItem
+                onClick={handleClose}
+                component={RouterLink}
+                to="/list"
+              >
+                <ListItemIcon>
+                  <ListAlt fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Shopping List</ListItemText>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <Settings fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Settings</ListItemText>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <Info fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>About</ListItemText>
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+        <Container sx={{ mt: 4 }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/list" element={<ShoppingListPage />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,21 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import itemsSlice from './slices/itemsSlice';
-import audioSlice from './slices/audioSlice';
 import uiSlice from './slices/uiSlice';
 
 export const store = configureStore({
   reducer: {
     items: itemsSlice,
-    audio: audioSlice,
     ui: uiSlice,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['audio/setMediaRecorder', 'audio/setAudioBlob'],
-        ignoredPaths: ['audio.mediaRecorder', 'audio.audioBlob'],
-      },
-    }),
 });
 
 export default store; 
